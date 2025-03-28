123253c8df161bf86f14e3d67678a8e774a7cbd0
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-03-28 12:34:44 +1100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PickRandomCards;
+
+namespace PickRandomCardsMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickCardsButton_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberOfCards.Text, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                PickedCards.Text = String.Empty;
+                foreach (string card in cards)
+                {
+                    PickedCards.Text += card + Environment.NewLine;
+
+                }
+                PickedCards.Text += Environment.NewLine + "You picked " + numberOfCards + " cards";
+            }
+            else
+            {
+                PickedCards.Text = "Please enter a valid number";
+            }
+        }
+
+        private void NumberOfCards_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
